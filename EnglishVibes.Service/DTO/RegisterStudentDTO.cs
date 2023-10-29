0c280c9ae6f6a216d57e733babbaf333247efdd2
Sob7yy <mustafam7moud7@gmail.com>
2023-10-30 00:01:34 +0200

    added DTOs

---
+++ EnglishVibes.Service/DTO/RegisterStudentDTO.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnglishVibes.Service.DTO
+{
+    public class RegisterStudentDTO
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Compare("Password")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+        public string PhoneNumber { get; set; }
+        public string? StudyPlan { get; set; }
+    }
+}

f13d64f9c58124517b8df05df7fc56d238e433cc
Sob7yy <mustafam7moud7@gmail.com>
2023-10-30 00:02:03 +0200

    added account and waitinglist controllers

---
+++ EnglishVibes.API/Controllers/AccountController.cs
@@ -0,0 +1,116 @@
+﻿using EnglishVibes.Data.Models;
+using EnglishVibes.Infrastructure.Data;
+using EnglishVibes.Service.DTO;
+using EnglishVibes.Service.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EnglishVibes.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly ApplicationDBContext context;
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        public AccountController
+            (ApplicationDBContext _context,
+                UserManager<ApplicationUser> _userManager,
+                SignInManager<ApplicationUser> _signInManager)
+        {
+            context = _context;
+            userManager = _userManager;
+            signInManager = _signInManager;
+        }
+
+        //[HttpGet]
+        //public async Task<ActionResult<IEnumerable<ApplicationUser>>> GetAll()
+        //{
+        //    return await context.Users.ToListAsync();
+        //}
+
+        // POST: api/Account/register
+        [HttpPost("register")]
+        //[ValidateAntiForgeryToken]
+        public async Task<ActionResult<Student>> RegisterStudent(RegisterStudentDTO studentDTO)
+        {
+            //if (_context.Students == null)
+            //{
+            //    return Problem("Entity set 'ApplicationDBContext.Students'  is null.");
+            //}
+            if (ModelState.IsValid)
+            {
+                var newStudent = new Student()
+                {
+                    UserName = studentDTO.UserName,
+                    Email = studentDTO.Email,
+                    PasswordHash = studentDTO.Password,
+                    PhoneNumber = studentDTO.PhoneNumber,
+                    StudyPlan = studentDTO.StudyPlan,
+                    ActiveStatus = false
+                };
+                IdentityResult result = await userManager.CreateAsync(newStudent, studentDTO.Password);
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(newStudent, false);
+                    return Ok(new { message = "Thank You For your registration. We will contact you shortly" });
+                }
+                else
+                {
+                    //ModelState.AddModelError("", result.Errors.FirstOrDefault().Description);
+                    return Problem(result.Errors.FirstOrDefault().Description);
+                }
+            }
+            return BadRequest(ModelState);
+            //_context.Students.Add(student);
+            //await _context.SaveChangesAsync();
+
+            //return CreatedAtAction("GetStudent", new { id = student.Id }, student);
+        }
+
+        // POST: api/Account/login
+        [HttpPost("login")]
+        //[ValidateAntiForgeryToken]
+        public async Task<ActionResult<Student>> Login(UserLoginDTO userLoginDTO)
+        {
+            //if (_context.Students == null)
+            //{
+            //    return Problem("Entity set 'ApplicationDBContext.Students'  is null.");
+            //}
+            if (ModelState.IsValid)
+            {
+                ApplicationUser appUser = await userManager.FindByNameAsync(userLoginDTO.UserName);
+                if (appUser != null)
+                {
+                    bool found = await userManager.CheckPasswordAsync(appUser, userLoginDTO.Password);
+                    if (found)
+                    {
+                        await signInManager.SignInAsync(appUser, userLoginDTO.RememberMe);
+                        //return Redirect("");
+                        return Ok(new { message = "Logged In Successfully" });
+                    }
+                    else
+                        return Problem("Incorrect UserName Or Password");
+                }
+                else
+                {
+                    return Problem("Incorrect UserName Or Password");
+                }
+            }
+            return BadRequest(ModelState);
+        }
+
+        // POST: api/Account/logout
+        [HttpGet("Logout")]
+        public async Task<ActionResult<Student>> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return Ok();
+        }
+    }
+}
---
+++ EnglishVibes.API/Controllers/WaitingListController.cs
@@ -0,0 +1,109 @@
+﻿using EnglishVibes.Data.Models;
+using EnglishVibes.Infrastructure.Data;
+using EnglishVibes.Service.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EnglishVibes.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WaitingListController : ControllerBase
+    {
+        private readonly ApplicationDBContext context;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public WaitingListController(ApplicationDBContext _context, UserManager<ApplicationUser> _userManager)
+        {
+            context = _context;
+            userManager = _userManager;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<WaitingListStudentDTO>>> GetWaitingList()
+        {
+            var inactiveStudents = await context.Students.Where(s => !s.ActiveStatus).ToListAsync();
+            List<WaitingListStudentDTO> waitingList = new List<WaitingListStudentDTO>();
+            foreach (Student student in inactiveStudents)
+            {
+                WaitingListStudentDTO waitingListStudent = new WaitingListStudentDTO()
+                {
+                    UserName = student.UserName,
+                    Email = student.Email,
+                    PhoneNumber = student.PhoneNumber,
+                    SelectedStudyPlan = student.StudyPlan
+                };
+                waitingList.Add(waitingListStudent);
+            }
+            return waitingList.ToList();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> CompleteStudentData(Guid id, CompleteStudentDataDTO studentData)
+        {
+            //if (id != student.Id)
+            //{
+            //    return BadRequest();
+            //}
+            if (ModelState.IsValid)
+            {
+                //var student = await userManager.FindByIdAsync(id);
+                var student = await context.Students.FindAsync(id);
+                if (student != null)
+                {
+                    student.CurrentLevel = studentData.Level;
+                    student.PayedAmount = studentData.PayedAmount;
+                    student.ActiveStatus = true;
+                    await context.SaveChangesAsync();
+                    if (student.StudyPlan == "private")
+                    {
+                        Group newGroup = new Group()
+                        {
+                            Level = studentData.Level,
+                            ActiveStatus = true
+                        };
+                        await context.Groups.AddAsync(newGroup);
+                        await context.SaveChangesAsync();
+                        var createdGroup = await context.Groups.OrderBy(g => g.Id).LastOrDefaultAsync();
+                        student.GroupId = createdGroup.Id;
+                    }
+                    else
+                    {
+                        var matchingGroup = await context.Groups
+                                .SingleOrDefaultAsync(g =>
+                                !g.ActiveStatus &&
+                                g.Level == studentData.Level &&
+                                g.Students.Count < 4);
+                        if (matchingGroup == null)
+                        {
+                            Group newGroup = new Group()
+                            {
+                                Level = studentData.Level,
+                                ActiveStatus = false
+                            };
+                            await context.Groups.AddAsync(newGroup);
+                            await context.SaveChangesAsync();
+                            var createdGroup = await context.Groups.OrderBy(g => g.Id).LastOrDefaultAsync();
+                            student.GroupId = createdGroup.Id;
+                        }
+                        else
+                        {
+                            student.GroupId = matchingGroup.Id;
+                            if (matchingGroup.Students.Count > 3)
+                            {
+                                matchingGroup.ActiveStatus = true;
+                            }
+                            await context.SaveChangesAsync();
+                        }
+                    }
+                    return Ok();
+                }
+                else
+                    return BadRequest();
+            }
+            return BadRequest(ModelState);
+        }
+    }
+}

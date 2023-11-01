19cc61897c736e81c24db73707dbb28ccfee38d9
Ahmed Mabrouk <dell@DESKTOP-5SJF271>
2023-11-01 10:54:02 +0200

    ..

---
+++ EnglishVibes.API/Helpers/ActiveGroupMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using EnglishVibes.API.DTO;
+using EnglishVibes.Data.Models;
+
+namespace EnglishVibes.API.Helpers
+{
+    public class ActiveGroupMappingProfile : Profile
+    {
+
+        public ActiveGroupMappingProfile()
+        {
+            CreateMap<Group, ActiveGroupDto>();
+        }
+    }
+}

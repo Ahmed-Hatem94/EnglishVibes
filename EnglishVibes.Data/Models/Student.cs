7fefeaa5bdc5012af232b73b702b5e63c17ccbe2
Ahmed Mabrouk <dell@DESKTOP-5SJF271>
2023-11-04 03:43:47 +0200

    student related data

--- EnglishVibes.Data/Models/Student.cs
+++ EnglishVibes.Data/Models/Student.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace EnglishVibes.Data.Models
@@ -14,6 +15,7 @@ namespace EnglishVibes.Data.Models
         public bool ActiveStatus { get; set; }
         public string? StudyPlan { get; set; }   // Private Or Group
         public int? GroupId { get; set; }        // Foreign Key
+        [JsonIgnore]
         public Group? Group { get; set; }
     }
 }

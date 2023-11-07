1bdb05db68adc274ad2efdf71694e2a4eb395a06
Sob7yy <mustafam7moud7@gmail.com>
2023-11-07 20:29:31 +0200

    added Repositories

---
+++ EnglishVibes.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,92 @@
+﻿using EnglishVibes.Data.Interfaces;
+using EnglishVibes.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnglishVibes.Infrastructure.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ApplicationDBContext dbContext;
+
+        public GenericRepository(ApplicationDBContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public virtual async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await dbContext.Set<T>().ToListAsync();
+        }
+
+        public virtual async Task<T> AddAsync(T entity)
+        {
+            await dbContext.Set<T>().AddAsync(entity);
+
+            return entity;
+        }
+
+        public async Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities)
+        {
+            await dbContext.Set<T>().AddRangeAsync(entities);
+            return entities;
+        }
+
+        public virtual T Update(T entity)
+        {
+            dbContext.Update(entity);
+
+            return entity;
+        }
+
+        public virtual void Delete(T entity)
+        {
+            dbContext.Set<T>().Remove(entity);
+        }
+
+        public void DeleteRange(IEnumerable<T> entities)
+        {
+            dbContext.Set<T>().RemoveRange(entities);
+        }
+
+        //public async Task SaveChangesAsync()
+        //{
+        //    await dbContext.SaveChangesAsync();
+        //}
+
+        public async Task<T> FindAsync(Expression<Func<T, bool>> criteria, string[] includes = null)
+        {
+            IQueryable<T> query = dbContext.Set<T>();
+
+            if (includes != null)
+            {
+                foreach (var include in includes)
+                {
+                    query = query.Include(include);
+                }
+            }
+
+            return await query.FirstOrDefaultAsync(criteria);
+        }
+
+        public async Task<IEnumerable<T>> FindAllAsync(Expression<Func<T, bool>> criteria, string[] includes = null)
+        {
+            IQueryable<T> query = dbContext.Set<T>();
+
+            if (includes != null)
+            {
+                foreach (var include in includes)
+                {
+                    query = query.Include(include);
+                }
+            }
+
+            return await query.Where(criteria).ToListAsync();
+        }
+    }
+}
